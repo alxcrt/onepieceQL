727f187e67fc3cc436f5bc96a18797c23b6e9cac
Alexandru Cretu <cretu.alexandru2000@gmail.com>
2024-08-05 19:36:50 +0300

    init project

---
+++ README.md
@@ -0,0 +1,3 @@
+# onepieceQL
+![](https://tenor.com/view/one-piece-one-piece-logo-logo-masterpiece-peak-fiction-gif-23559694.gif)
+
---
+++ src/server/src/index.ts
@@ -0,0 +1,19 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { logger } from "hono/logger";
+
+const app = new Hono();
+
+app.use(logger());
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+const port = 3000;
+console.log(`Server is running on port ${port}`);
+
+serve({
+  fetch: app.fetch,
+  port,
+});
